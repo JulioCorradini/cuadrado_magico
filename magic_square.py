9276264ced9c27b1bee09dce29664b5f14d62de8
Julio Corradini <julcorradi@gmail.com>
2024-03-06 22:14:42 -0300

    Creando el cuadro. Mapeando cada posición con una clave de un diccionario. Poblando cada clave (que representa una posición en el cuadro) con un valor aleatorio.

--- magic_square.py
+++ magic_square.py
@@ -1,10 +1,33 @@
+import random
+
 square_size = 3
 
 def create_square(square_size):
+    
+    position = 0
+    positions = {}
+    value = 0
+
+    # Creo las columnas del cuadrado.
     square = list(range(square_size))
+
+    # Creo las filas del cuadrado.
     for row in square:
         square[row] = list(range(square_size))
     for row in square:
      print(row)
+
+    # Pueblo el diccionario con las claves que representan cada una de las posiciones del cuadrado y las inicializo a todas en 0.
+    for column in square:
+        for row in column:
+           position += 1
+           positions.setdefault(position, 0)
+    
+    # Pueblo cada clave con un valor aleatorio.
+    for position in positions:
+       value = random.randint(1, 10)
+       positions[position] = value
+
+    print(positions)
 
 create_square(square_size)
