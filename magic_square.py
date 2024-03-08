63b90caa16fe763668cc4d4c684ebf979a0c7735
Julio Corradini <julcorradi@gmail.com>
2024-03-07 22:23:48 -0300

    Conectando el diccionario con el cuadrado, para que el mismo presente los valores del primero.

--- magic_square.py
+++ magic_square.py
@@ -1,21 +1,19 @@
 import random
 
-square_size = 3
+# Tamaño del cuadrado.
+square_size = 6
 
 def create_square(square_size):
     
+    # Creo las variables necesarias.
     position = 0
     positions = {}
     value = 0
 
-    # Creo las columnas del cuadrado.
+    # Creo el cuadrado.
     square = list(range(square_size))
-
-    # Creo las filas del cuadrado.
     for row in square:
         square[row] = list(range(square_size))
-    for row in square:
-     print(row)
 
     # Pueblo el diccionario con las claves que representan cada una de las posiciones del cuadrado y las inicializo a todas en 0.
     for column in square:
@@ -28,11 +26,16 @@ def create_square(square_size):
        value = random.randint(1, 10)
        positions[position] = value
     
-    # Pintando el cuadrado con los valores del diccionario.
-    for cell in positions:
-        
-          
-
+    # Imprimo el diccionario.
     print(positions)
+    
+    # Pintando el cuadrado con los valores del diccionario.
+    for row in range(square_size):
+       for column in range(square_size):
+          square[row][column] = positions[(row * square_size + column) + 1]
+    
+    # Imprimimos el cuadrado.
+    for row in square:
+       print(row)
 
 create_square(square_size)
